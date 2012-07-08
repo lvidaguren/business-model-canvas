eeddbc544d277f363578d07e2e4c0c8dc78e2457
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-07-08 22:41:55 +0200

    Adding a back button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,4 @@ class HomeController < ApplicationController
     board.update_attribute(:key, Base32::Crockford.encode(board.id))
     redirect_to board_path(board.key)
   end
-  
-  def save
-    session[params[:id].to_sym] = params[:value] 
-    render :text => RedCloth.new(params[:value]).to_html
-  end
-  
-  def load
-    render :text => session[params[:id].to_sym]
-  end
-  
 end
