cd42826c0e9cc7b58198936df743bfa151941a39
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-07-30 00:17:00 +0200

    Fixing flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :set_locale, :authenticate_user
+  before_filter :board_warning
+  
   helper_method :current_board
   
   def after_sign_in_path_for(resource)
@@ -26,5 +28,9 @@ class ApplicationController < ActionController::Base
       flash[:notice] = t('.private_board')
       redirect_to root_path  
     end
+  end
+  
+  def board_warning
+    current_board.public? && controller_name == "boards" ? flash[:warning] = t('.warning.your_board_is_public') : flash.delete(:warning)
   end
 end
