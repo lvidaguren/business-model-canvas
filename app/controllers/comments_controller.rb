a070ad6af6440496abdd7d439f1c3845af81f1ea
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-08-05 04:03:01 +0200

    Fixes comments ordering

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,28 @@
 class CommentsController < ApplicationController
+  before_filter :load_card
+  
   def index
-    @card = current_board.cards.find(params[:card_id])
-    @comments = @card.comment_threads
+    @comments = @card.comment_threads.order(:created_at)
   end
   
   def create
-    @card = current_board.cards.find(params[:comment][:commentable_id])
     @comment = Comment.new(params[:comment])
     @comment.commentable = @card
     @comment.user = current_user
     @comment.save!
     
-    @comments = @card.comment_threads
+    @comments = @card.comment_threads.order(:created_at)
   end
   
   def destroy
-    @card = current_board.cards.find(params[:card_id])
     comment = @card.comment_threads.find(params[:id])
     comment.destroy
     
-    @comments = @card.comment_threads
+    @comments = @card.comment_threads.order(:created_at)
+  end
+  
+  protected
+  def load_card
+    @card = current_board.cards.find(params[:card_id])
   end
 end
